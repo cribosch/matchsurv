2657cab9ae737c1f4fcc6361bdf5d9831730a1c0
cribosch <cribosch@users.noreply.github.com>
2017-05-05 15:34:28 +0200

    Update erpsm.R

--- R/erpsm.R
+++ R/erpsm.R
@@ -419,7 +419,7 @@ predictErpsd <- function(jumptime, S0, weight, beta, time=NULL,...){
 ##' @export
 predict.erpsd <- function(object, data, time=object$exit,strata=object$strata,...){
 	browser()
-	if (!is.null(strata) && !all.equal(time, object$exit)) {
+	if (!is.null(strata) && time!=object$exit) {
 		time0<-time
 		time<-rep(list(time0), nlevel(strata))
 			  }
