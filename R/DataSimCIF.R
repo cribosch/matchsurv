c3090473ebd067fcc2f83c9cead03bb062d5dafd
cribosch <cri.boschini@gmail.com>
2019-03-28 13:30:08 +0100

    debug

--- R/DataSimCIF.R
+++ R/DataSimCIF.R
@@ -199,9 +199,8 @@ simexpo<-function(F0,
   # }
   
   if (biass){ 
-    #age<-(5+runif(1)*2)*(X1==1)+(X1==0)*((20+runif(1)*3))
-    age<-0
-  } else #age<-runif(1,5,25)
+    age<-(5+runif(1)*2)*(X1==1)+(X1==0)*((20+runif(1)*3))
+  } else age<-0 #age<-runif(1,5,25)
   
   entry<-age
   
